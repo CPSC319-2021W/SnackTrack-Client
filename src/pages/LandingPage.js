5d7245b9293c724c7340f9bef30050772ec9d4f0
Hyesun An <anhyesun@gmail.com>
2021-02-02 14:55:47 -0800

    Set up router and routes for main

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Landing = () => (
+  <div>
+    <p>
+      Landing Page
+    </p>
+  </div>
+);
+export default Landing;
---
+++ src/pages/TransactionsHistory.js
@@ -0,0 +1,9 @@
+import React from "react";
+const TransactionsHistory = () => (
+  <div>
+    <p>
+      <code>Transactions History page</code>
+    </p>
+  </div>
+);
+export default TransactionsHistory;
