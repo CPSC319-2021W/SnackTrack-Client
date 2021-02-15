2410571b287f2e3d36553936e006509d8a364f4e
Justin Wong <justinc.s.wong@gmail.com>
2021-02-15 02:45:18 -0800

    SNAK-87-Add TODO for catch-block

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -5,6 +5,7 @@ const getUserById = async (userId) => {
     const response = await httpClient.get(`/users/${userId}`);
     return response.data;
   } catch (err) {
+    // TODO: Handle 404
     console.log(err.toString());
   }
 };
