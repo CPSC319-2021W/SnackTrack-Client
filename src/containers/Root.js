acf615ecd5dfc7248bd489b73db853f87d3d8281
angelli <angellidimatulac@gmail.com>
2021-02-15 11:59:48 -0800

    Add fonts, clean up root

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,4 +1,4 @@
-import '../index.css';
+import '../styles/Global.css';
 
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import { setProfile, setToken } from '../redux/features/auth/authSlice';
@@ -21,37 +21,35 @@ const Root = () => {
   const authSetProfile = (profile) => dispatch(setProfile(profile));
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <Router>
-          <Switch>
-            <Route path='/auth-login' component={AuthLogin} />
-            <Route path='/select-login' component={SelectLogin} />
-            <Route path='/snack-list' component={SnackList} />
-            <Route
-              path='/transaction-history'
-              component={TransactionHistory}/>
-            <Route path='/user-profile' component={UserProfile} />
-            <Route exact path='/' component={Landing} />
-          </Switch>
-        </Router>
-        <div>Token: {token}</div>
-        <div>
-          Profile: {profile ? `Welcome, ${profile.username}` : ''}
-        </div>
-        <button onClick={() => authSetToken('fake_token')}>
-          Set Token
-        </button>
-        <button
-          onClick={() =>
-            authSetProfile({ id: 1, username: 'FakeUsername' })
-          }>
-          Set Profile
-        </button>
-        <p>
-          <code>brb building things</code>
-        </p>
-      </header>
+    <div className='root-container'>
+      <Router>
+        <Switch>
+          <Route path='/auth-login' component={AuthLogin} />
+          <Route path='/select-login' component={SelectLogin} />
+          <Route path='/snack-list' component={SnackList} />
+          <Route
+            path='/transaction-history'
+            component={TransactionHistory}/>
+          <Route path='/user-profile' component={UserProfile} />
+          <Route exact path='/' component={Landing} />
+        </Switch>
+      </Router>
+      <div>Token: {token}</div>
+      <div>
+        Profile: {profile ? `Welcome, ${profile.username}` : ''}
+      </div>
+      <button onClick={() => authSetToken('fake_token')}>
+        Set Token
+      </button>
+      <button
+        onClick={() =>
+          authSetProfile({ id: 1, username: 'FakeUsername' })
+        }>
+        Set Profile
+      </button>
+      <p>
+        <code>brb building things</code>
+      </p>
     </div>
   );
 };
