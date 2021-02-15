b9bd18d001040035981c2705c7000e4b055e1033
Justin Wong <justinc.s.wong@gmail.com>
2021-02-14 21:54:22 -0800

    SNAK-74-Add Snack Grid component

---
+++ src/components/SnackGrid.js
@@ -0,0 +1,20 @@
+import { Grid } from '@material-ui/core';
+import React from 'react';
+import SnackCard from './SnackCard';
+
+const SnackGrid = (props) => {
+  const { snacks, onClick } = props;
+  return (
+    <Grid container spacing={5}>
+      {snacks.map((snack, index) => {
+        return (
+          <Grid key={index} item lg={3} sm={6}>
+            <SnackCard snack={snack} onClick={() => onClick(snack.snackName)} />
+          </Grid>
+        );
+      })}
+    </Grid>
+  );
+};
+
+export default SnackGrid;
