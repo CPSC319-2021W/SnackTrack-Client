b33615ccab839a25ed8b3e76c3cb4a2bd1ec7719
Justin Wong <justinc.s.wong@gmail.com>
2021-02-17 00:22:28 -0800

    SNAK-89-Render TransactionTable Component to Transactions route

--- src/pages/Transactions.js
+++ src/pages/Transactions.js
@@ -1,26 +1,39 @@
 import { React, useState } from 'react';
 
 import PaymentsTable from '../components/PaymentsTable';
+import TransactionsTable from '../components/TransactionsTable';
 import { getPaymentHistory } from '../services/PaymentsService';
+import { getTransactions } from '../services/TransactionsService';
 
 const rowsPerPage = 8;
 
 const Transactions = () => {
   const [paymentHistory, setPaymentHistory] = useState(getPaymentHistory(0, rowsPerPage));
+  const [transactions, setTransactions] = useState(getTransactions(0, rowsPerPage));
 
-  const handleChangePage = (page) => {
+  const handlePaymentChangePage = (page) => {
     setPaymentHistory(getPaymentHistory(page, rowsPerPage));
+  };
+
+  const handleTransactionChangePage = (page) => {
+    setTransactions(getTransactions(page, rowsPerPage));
   };
 
   return (
     <div>
       <p>
-        <code>Past Transactions</code>
+        <code>Orders</code>
       </p>
+
+      <TransactionsTable
+        data={transactions}
+        rowsPerPage={rowsPerPage}
+        onChangePage={handleTransactionChangePage}
+      />
       <PaymentsTable
         data={paymentHistory}
         rowsPerPage={rowsPerPage}
-        onChangePage={handleChangePage}
+        onChangePage={handlePaymentChangePage}
       />
     </div>
   );
