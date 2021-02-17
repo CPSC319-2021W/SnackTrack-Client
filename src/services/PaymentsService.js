b3793dc3970d38d0a96671a1e40d2ed26edf8c0e
Justin Wong <justinc.s.wong@gmail.com>
2021-02-17 00:21:36 -0800

    SNAK-89-Refactor mockService for mock transactions

--- src/services/PaymentsService.js
+++ src/services/PaymentsService.js
@@ -1,9 +1,9 @@
 // import httpClient from './axios.config.js';
-import { mockDBCall } from '../mockServer';
+import { mockPaymentDBCall } from '../mockServer';
 
 const getPaymentHistory = (page, rowsPerPage) => {
   // TODO: Replace with API Call (Ticket: SNAK-126)
-  return mockDBCall(page, rowsPerPage);
+  return mockPaymentDBCall(page, rowsPerPage);
 };
 
 export { getPaymentHistory };
