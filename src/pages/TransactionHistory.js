c144ba24d84a9e1d76a2c1f240058c2a7bda720c
Justin Wong <justinc.s.wong@gmail.com>
2021-02-16 17:34:19 -0800

    SNAK-90-Add PaymentsTable to transactions page

--- src/pages/TransactionHistory.js
+++ src/pages/TransactionHistory.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import { React, useState } from 'react';
 
-const TransactionHistory = () => (
-  <div>
-    <p>
-      <code>Transaction History page</code>
-    </p>
-  </div>
-);
+import PaymentsTable from '../components/PaymentsTable';
+import { getPaymentHistory } from '../services/PaymentsService';
+
+const rowsPerPage = 8;
+
+const TransactionHistory = () => {
+  const [paymentHistory, setPaymentHistory] = useState(getPaymentHistory(0, rowsPerPage));
+
+  const handleChangePage = (page) => {
+    setPaymentHistory(getPaymentHistory(page, rowsPerPage));
+  };
+
+  return (
+    <div>
+      <p>
+        <code>Past Transactions</code>
+      </p>
+      <PaymentsTable
+        data={paymentHistory}
+        rowsPerPage={rowsPerPage}
+        onChangePage={handleChangePage}
+      />
+    </div>
+  );
+};
 export default TransactionHistory;
