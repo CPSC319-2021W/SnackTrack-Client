b6eff4dc6455e1562e311170e9a08991d15ef542
Justin Wong <justinc.s.wong@gmail.com>
2021-02-16 17:44:35 -0800

    SNAK-90-Add TODOs

--- src/services/PaymentsService.js
+++ src/services/PaymentsService.js
@@ -2,7 +2,7 @@
 import { mockDBCall } from '../mockServer';
 
 const getPaymentHistory = (page, rowsPerPage) => {
-  // TODO: Replace with DB Call
+  // TODO: Replace with API Call (Ticket: SNAK-126)
   return mockDBCall(page, rowsPerPage);
 };
 
