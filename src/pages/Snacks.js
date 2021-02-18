8599d1374d773bdc2c28a607ff679b2725f99e88
Justin Wong <justinc.s.wong@gmail.com>
2021-02-18 10:27:56 -0800

    SNAK-118-Add snack to Snacks route for logged in feedback

--- src/pages/Snacks.js
+++ src/pages/Snacks.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import SnackCard from '../components/SnackCard';
 
 const Snacks = () => (
   <div>
     <p>Snacks Page</p>
+    <SnackCard
+      snack={{
+        imageUri:
+          'https://www.hersheys.com/content/dam/smartlabelproductsimage/kitkat/00034000002467-0010.png',
+        snackName: 'KitKat',
+        price: 200
+      }}
+      onClick={alert}
+    />
   </div>
 );
 
