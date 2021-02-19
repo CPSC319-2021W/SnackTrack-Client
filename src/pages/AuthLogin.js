ef2d6ea46103aa8d9cd39231ee745b4380f14578
Angelli Dimatulac <44279603+adimatulac@users.noreply.github.com>
2021-02-19 15:56:29 -0800

    SNAK-154: Implement Layout component for global HeaderBar and styles (#33)
    * Added Layout component
    
    * Adding push to history back in
    
    * Cleaning up files
    
    * Removed extra line for imports
    
    Co-authored-by: Lorenzo Bisceglia <lorenzo.bisceglia9@gmail.com>

--- src/pages/AuthLogin.js
+++ src/pages/AuthLogin.js
@@ -7,7 +7,7 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 import appStyles from '../styles/SnackTrack.module.css';
 import { setLoginSuccess } from '../redux/features/auth/authSlice';
-import styles from '../styles/AuthLogin.module.css';
+import styles from '../styles/Login.module.css';
 import { useGoogleLogin } from 'react-google-login';
 
 const AuthLogin = () => {
@@ -28,7 +28,7 @@ const AuthLogin = () => {
   });
 
   return (
-    <div>
+    <div className={styles.container}>
       {profile?.token ? <Redirect to='/snacks' /> : null}
       <Card className={styles.card}>
         <img className={styles.none} src={GalvanizeLogo} alt='Galvanize Logo' />
