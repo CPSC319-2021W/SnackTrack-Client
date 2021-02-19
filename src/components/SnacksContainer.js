267e3b2f00a7aa11ebb14590a87127690c23e4c9
Hyesun An <anhyesun@gmail.com>
2021-02-19 15:04:09 -0800

    remove container

--- src/components/SnacksContainer.js
+++ src/components/SnacksContainer.js
@@ -1,5 +1,4 @@
 import CategoryFilter from './CategoryFilter';
-import { Container } from '@material-ui/core';
 import React from 'react';
 import SnackGrid from './SnackGrid';
 import {snacks} from '../mockSnacks';
@@ -11,37 +10,31 @@ const SnacksContainer = () => {
   const currFilter = snackFilter.categories;
   return (
     <div>
-      <Container>
-        <div className={styles.header}>
-          <div className={styles.leftBox}>
-            <p> Snacks List </p>
+      <div className={styles.header}>
+        <div className={styles.leftBox}>
+          <p> Snacks List </p>
+        </div>
+        <div className={styles.suggestBox}>
+          <div className={styles.suggestBoxQ}>
+            Can&apos;t find what you want?
           </div>
-          <div className={styles.suggestBox}>
-            <div className={styles.suggestBoxQ}>
-              Can&apos;t find what you want?
-            </div>
-            <div className={styles.suggestBoxLink}>
-              <a className={styles.a} href='http://localhost:3000/'>
-                Suggest a snack!
-              </a>
-            </div>
+          <div className={styles.suggestBoxLink}>
+            <a className={styles.a} href='http://localhost:3000/'>
+              Suggest a snack!
+            </a>
           </div>
         </div>
-      </Container>
-      <Container>
-        <CategoryFilter />
-        <div className={styles.snackGrid}>
-          {(currFilter.length === 0) ? (
-            <><SnackGrid snacks={snacks.snacks} onClick={alert} />
-            </>
-          ) : (
-            <><SnackGrid snacks={snacks.snacks.filter((item) => {
-              return currFilter.includes(item.snack_type);
-            })} onClick={alert} />
-            </>
-          )}
-        </div>
-      </Container>
+      </div>
+      <CategoryFilter />
+      <div className={styles.snackGrid}>
+        {(currFilter.length === 0) ? (
+          <SnackGrid snacks={snacks.snacks} onClick={alert} />
+        ) : (
+          <SnackGrid snacks={snacks.snacks.filter((item) => {
+            return currFilter.includes(item.snack_type);
+          })} onClick={alert} />
+        )}
+      </div>
     </div>
   );
 };
