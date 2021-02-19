8de717b87bef7b05e9c474437bd433e3058647bf
Justin Wong <justinc.s.wong@gmail.com>
2021-02-19 00:41:13 -0800

    SNAK-115-Add Adornment/Icon

--- src/components/UserSearchBar.js
+++ src/components/UserSearchBar.js
@@ -2,6 +2,7 @@ import { Input, InputAdornment } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
 
 import React from 'react';
+import search from '../assets/icons/search.svg';
 import { setValue } from '../redux/features/searchbar/searchbarSlice';
 import styles from '../styles/UserSearchBar.module.css';
 
@@ -18,7 +19,11 @@ const UserSearchBar = () => {
       className={styles.searchBar}
       autoFocus={true}
       disableUnderline={true}
-      startAdornment={<InputAdornment position='start'>$</InputAdornment>}
+      startAdornment={
+        <InputAdornment position='start' variant='filled'>
+          <img className={styles.icon__base} src={search} />
+        </InputAdornment>
+      }
       placeholder='Enter your name...'
       value={searchValue}
       onChange={handleChange}
