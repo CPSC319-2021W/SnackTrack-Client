0c32a96f25c26e9cbe90d6246cfff1c9b6d05dc1
angelli <angellidimatulac@gmail.com>
2021-02-20 08:40:20 -0800

    Move headings to page

--- src/components/SnacksList/SnacksContainer.js
+++ src/components/SnacksList/SnacksContainer.js
@@ -1,26 +1,12 @@
 import CategoryFilter from './CategoryFilter';
 import React from 'react';
 import SnackGrid from './SnackGrid';
-import styles from '../../styles/SnacksContainer.module.css';
 
 const SnacksContainer = (props) => {
   const { snacks, filters } = props;
 
   return (
     <div>
-      <div className={styles.header}>
-        <div className={styles.leftBox}>
-          <p> Snacks </p>
-        </div>
-        <div className={styles.suggestBox}>
-          <div className={styles.suggestBoxQ}>{"Can't find what you want?"}</div>
-          <div className={styles.suggestBoxLink}>
-            <a className={styles.a} href='http://localhost:3000/'>
-              Suggest a snack!
-            </a>
-          </div>
-        </div>
-      </div>
       <CategoryFilter />
       <div>
         {filters.length === 0 ? (
--- src/pages/Snacks.js
+++ src/pages/Snacks.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import { fetchSnacks } from '../redux/features/snacks/snacksSlice';
 import SnacksContainer from '../components/SnacksList/SnacksContainer';
+import styles from '../styles/Snacks.module.css';
 
 const Snacks = () => {
   const dispatch = useDispatch();
@@ -14,6 +15,19 @@ const Snacks = () => {
 
   return (
     <div>
+      <div className={styles.header}>
+        <div className={styles.leftBox}>
+          <p> Snacks </p>
+        </div>
+        <div className={styles.suggestBox}>
+          <div className={styles.suggestBoxQ}>{"Can't find what you want?"}</div>
+          <div className={styles.suggestBoxLink}>
+            <a className={styles.a} href='http://localhost:3000/'>
+              Suggest a snack!
+            </a>
+          </div>
+        </div>
+      </div>
       <SnacksContainer snacks={snacks} filters={categories} />
     </div>
   );
