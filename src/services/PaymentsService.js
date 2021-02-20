80d4749ac673fd263db7cec63fc60bf8e82c6fa9
Justin Wong <justinc.s.wong@gmail.com>
2021-02-20 04:04:18 -0800

    SNAK-188-Add TODO

--- src/services/PaymentsService.js
+++ src/services/PaymentsService.js
@@ -1,6 +1,7 @@
 import httpClient from './axios.config.js';
 
 const getPaymentHistory = async (page, rowsPerPage) => {
+  // TODO: Change to correct call for a user's payment history, maybe move to UsersService? (SNAK-196)
   try {
     const response = await httpClient.get(`/payments/?page=${page}&size=${rowsPerPage}`);
     return response.data;
