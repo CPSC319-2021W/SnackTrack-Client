3eaa1648545de1e09b324a6800d6b3d64d8c3d37
Justin Wong <justinc.s.wong@gmail.com>
2021-02-19 19:21:09 -0800

    SNAK-155-Refactor Mock Data

---
+++ src/mockPaymentData.js
@@ -0,0 +1,22 @@
+const mockPaymentData = [
+  { paymentDate: '2020/01/21', paymentAmount: 500, processedBy: 'JustinWong' },
+  { paymentDate: '2020/01/21', paymentAmount: 500, processedBy: 'JustinWong' },
+  { paymentDate: '2020/01/21', paymentAmount: 500, processedBy: 'HyesunAn' },
+  { paymentDate: '2020/01/12', paymentAmount: 1000, processedBy: 'JustinWong' },
+  { paymentDate: '2020/01/12', paymentAmount: 1000, processedBy: 'JustinWong' },
+  { paymentDate: '2020/01/12', paymentAmount: 1000, processedBy: 'JustinWong' },
+  { paymentDate: '2020/01/10', paymentAmount: 100, processedBy: 'JustinWong' },
+  { paymentDate: '2020/01/10', paymentAmount: 100, processedBy: 'JustinWong' },
+  { paymentDate: '2020/01/10', paymentAmount: 100, processedBy: 'JustinWong' },
+  { paymentDate: '2020/01/10', paymentAmount: 25, processedBy: 'JustinWong' },
+  { paymentDate: '2020/01/10', paymentAmount: 25, processedBy: 'JustinWong' },
+  { paymentDate: '2020/01/10', paymentAmount: 25, processedBy: 'JustinWong' },
+  { paymentDate: '2020/01/01', paymentAmount: 125, processedBy: 'JustinWong' },
+  { paymentDate: '2020/01/01', paymentAmount: 125, processedBy: 'JustinWong' },
+  { paymentDate: '2020/01/01', paymentAmount: 125, processedBy: 'JustinWong' },
+  { paymentDate: '2020/01/01', paymentAmount: 125, processedBy: 'JustinWong' },
+  { paymentDate: '2020/01/01', paymentAmount: 125, processedBy: 'JustinWong' },
+  { paymentDate: '2020/01/01', paymentAmount: 125, processedBy: 'JustinWong' }
+];
+
+export default mockPaymentData;
