b7aa3ff773fd3ef01472b14eb950ddfa2f5a9cda
Justin Wong <justinc.s.wong@gmail.com>
2021-02-19 16:26:16 -0800

    SNAK-76-Refactor snacks to be passed into SnackContainer as props

--- src/components/SnacksContainer.js
+++ src/components/SnacksContainer.js
@@ -4,10 +4,11 @@ import SnackGrid from './SnackGrid';
 import styles from '../styles/SnacksContainer.module.css';
 import { useSelector } from 'react-redux';
 
-const SnacksContainer = (props) => {  
-  const {snacks} = props;
+const SnacksContainer = (props) => {
+  const { snacks } = props;
   const snackFilter = useSelector((state) => state.snackReducer);
   const currFilter = snackFilter.categories;
+
   return (
     <div>
       <div className={styles.header}>
@@ -15,9 +16,7 @@ const SnacksContainer = (props) => {
           <p> Snacks List </p>
         </div>
         <div className={styles.suggestBox}>
-          <div className={styles.suggestBoxQ}>
-            Can&apos;t find what you want?
-          </div>
+          <div className={styles.suggestBoxQ}>{"Can't find what you want?"}</div>
           <div className={styles.suggestBoxLink}>
             <a className={styles.a} href='http://localhost:3000/'>
               Suggest a snack!
@@ -27,15 +26,19 @@ const SnacksContainer = (props) => {
       </div>
       <CategoryFilter />
       <div>
-        {(currFilter.length === 0) ? (
+        {currFilter.length === 0 ? (
           <SnackGrid snacks={snacks} onClick={alert} />
         ) : (
-          <SnackGrid snacks={snacks.filter((item) => {
-            return currFilter.includes(item.snack_type);
-          })} onClick={alert} />
+          <SnackGrid
+            snacks={snacks.filter((item) => {
+              return currFilter.includes(item.snack_type);
+            })}
+            onClick={alert}
+          />
         )}
       </div>
     </div>
   );
 };
+
 export default SnacksContainer;
