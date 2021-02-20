71b066c1772867ea1350a3542ba002e3e212b75d
Justin Wong <justinc.s.wong@gmail.com>
2021-02-20 01:45:35 -0800

    SNAK-189-Added call to API with loading state

--- src/pages/AuthLogin.js
+++ src/pages/AuthLogin.js
@@ -1,8 +1,8 @@
-import { Button, Card } from '@material-ui/core';
+import { Button, Card, CircularProgress } from '@material-ui/core';
+import React, { useState } from 'react';
 import { loginFailure, refreshTokenSetup } from '../helpers/AuthLoginHelper';
 
 import GalvanizeLogo from '../images/logo/galvanize.svg';
-import React from 'react';
 import appStyles from '../styles/SnackTrack.module.css';
 import { authenticate } from '../services/UserService';
 import { setUser } from '../redux/features/users/usersSlice';
@@ -14,14 +14,17 @@ import { useHistory } from 'react-router-dom';
 const AuthLogin = () => {
   const dispatch = useDispatch();
   const history = useHistory();
+  const [isLoading, setIsLoading] = useState(false);
   const setProfile = (profile) => dispatch(setUser(profile));
 
   const onSuccess = async (googleUser) => {
+    setIsLoading(true);
     const token = googleUser.getAuthResponse().id_token;
     const userResponse = await authenticate(token);
     setProfile(userResponse);
     refreshTokenSetup(googleUser);
     history.push('/snacks');
+    setIsLoading(false);
   };
 
   const { signIn } = useGoogleLogin({
@@ -31,13 +34,22 @@ const AuthLogin = () => {
     isSignedIn: true
   });
 
+  const handleLogIn = () => {
+    setIsLoading(true);
+    signIn();
+  };
+
   return (
     <div className={styles.container}>
       <Card className={styles.card}>
         <img className={styles.none} src={GalvanizeLogo} alt='Galvanize Logo' />
         <h2 className={appStyles.SnackTrack}>SnackTrack</h2>
-        <Button className={styles.button__login} variant='outlined' onClick={signIn}>
-          Log in with Google
+        <Button className={styles.button__login} variant='outlined' onClick={handleLogIn}>
+          {isLoading ? (
+            <CircularProgress size={30} thickness={5} />
+          ) : (
+            'Log in with Google'
+          )}
         </Button>
       </Card>
     </div>
