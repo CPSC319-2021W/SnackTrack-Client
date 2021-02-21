d864c8fec914455b8503fa413d23b37b26cac05f
angelli <angellidimatulac@gmail.com>
2021-02-21 09:42:38 -0800

    Clead up pages and added error state for PaymentsTable

--- src/components/OrdersTable/RenderOrdersTable.js
+++ src/components/OrdersTable/RenderOrdersTable.js
@@ -5,6 +5,7 @@ import {
   Table,
   TableBody,
   TableCell,
+  TableContainer,
   TableFooter,
   TableHead,
   TablePagination,
@@ -14,7 +15,7 @@ import { isCancelled, isPaid, isPaymentPending } from '../../helpers/OrdersHelpe
 
 import React from 'react';
 import classNames from 'classnames';
-import styles from '../../styles/OrdersTable.module.css';
+import styles from '../../styles/Table.module.css';
 
 const RenderOrdersTable = (props) => {
   const {
@@ -108,112 +109,114 @@ const RenderOrdersTable = (props) => {
 
   return (
     <Card className={styles.paper}>
-      <Table className={styles.table} aria-label='Orders Table'>
-        <TableHead>
-          <TableRow className={styles.header}>
-            <TableCell className={styles.primaryHeader} colSpan={columns.length - 1}>
-              <h3 className={styles.primaryHeader__text}>Orders</h3>
-            </TableCell>
-            <TableCell className={styles.cell__pay}>
-              <Button
-                className={styles.button__pay}
-                disabled={payForOrdersDisabled}
-                onClick={() => onPayForOrders()}
-              >
-                Pay for
-                { selectedOrders.length > 1
-                  ? ` ${ selectedOrders.length } Orders`
-                  : ' Order' }
-              </Button>
-            </TableCell>
-          </TableRow>
-          <TableRow className={styles.header__row}>
-            {columns.map((column) => (
-              <TableCell
-                key={column.id}
-                className={`${styles.secondaryHeader} ${styles.cell}`}
-              >
-                <h4 className={styles.secondaryHeader__text}>{column.label}</h4>
+      <TableContainer>
+        <Table className={styles.table} aria-label='Orders Table'>
+          <TableHead>
+            <TableRow className={styles.header}>
+              <TableCell className={styles.primaryHeader} colSpan={columns.length - 1}>
+                <h4 className={styles.primaryHeader__text}>Orders</h4>
               </TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {transactions.map((order, i) => {
-            return (
-              <TableRow key={i} tabIndex={-1} className={classNames({
-                [styles.row]: !checkIsSelected(transactions[i].transaction_id),
-                [styles.row__selected]: checkIsSelected(transactions[i].transaction_id)
-              })}>
-                {columns.map((column) => {
-                  const value = order[column.id];
-                  return (
-                    <TableCell
-                      key={column.id}
-                      className={`${styles.cell} ${styles.cell__small} ${
-                        column.id === 'transaction_type_id' ? styles.cell__large : null
-                      } ${column.id !== 'checkbox' ? styles.cell__medium : null}`}
-                    >
-                      {column.id === 'transaction_type_id'
-                        ? column.format(value, order.payment_id)
-                        : column.format && typeof value === 'number'
-                          ? column.format(value)
-                          : value}
-                      {column.id === 'checkbox' &&
-                      isPaymentPending(
-                        transactions[i].payment_id,
-                        transactions[i].transaction_type_id
-                      ) ? (
-                          <Checkbox
-                            size='small'
-                            checked={checkIsSelected(transactions[i].transaction_id)}
-                            onClick={() =>
-                              onSelectOrder(
-                                transactions[i].transaction_id,
-                                transactions[i].transaction_amount
-                              )
-                            }
-                          />
-                        ) : null}
-                      {column.id === 'actions' &&
-                      isPaymentPending(
-                        transactions[i].payment_id,
-                        transactions[i].transaction_type_id
-                      ) ? (
-                          <Button className={styles.button__edit}>
-                           Edit Order
-                          </Button>
-                        ) : null}
-                    </TableCell>
-                  );
-                })}
-              </TableRow>
-            );
-          })}
-          {emptyRows().map((row) => {
-            return (
-              <TableRow key={row} tabIndex={-1}>
-                {columns.map((column) => {
-                  return <TableCell key={column.id} className={styles.cell} />;
-                })}
-              </TableRow>
-            );
-          })}
-        </TableBody>
-        <TableFooter>
-          <TableRow>
-            <TablePagination
-              className={styles.pagination}
-              count={totalRows}
-              page={currentPage}
-              rowsPerPage={rowsPerPage}
-              labelDisplayedRows={({ page }) => `Page ${page + 1} of ${totalPages}`}
-              rowsPerPageOptions={[rowsPerPage]}
-              onChangePage={(event, page) => onChangePage(page)}
-            />
-          </TableRow>
-        </TableFooter>
-      </Table>
+              <TableCell className={styles.cell__pay}>
+                <Button
+                  className={styles.button__pay}
+                  disabled={payForOrdersDisabled}
+                  onClick={() => onPayForOrders()}
+                >
+                  Pay for
+                  { selectedOrders.length > 1
+                    ? ` ${ selectedOrders.length } Orders`
+                    : ' Order' }
+                </Button>
+              </TableCell>
+            </TableRow>
+            <TableRow className={styles.header__row}>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  className={`${styles.secondaryHeader} ${styles.cell}`}
+                >
+                  <h6 className={styles.secondaryHeader__text}>{column.label}</h6>
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {transactions.map((order, i) => {
+              return (
+                <TableRow key={i} tabIndex={-1} className={classNames({
+                  [styles.row]: !checkIsSelected(transactions[i].transaction_id),
+                  [styles.row__selected]: checkIsSelected(transactions[i].transaction_id)
+                })}>
+                  {columns.map((column) => {
+                    const value = order[column.id];
+                    return (
+                      <TableCell
+                        key={column.id}
+                        className={`${styles.cell} ${styles.cell__small} ${
+                          column.id === 'transaction_type_id' ? styles.cell__large : null
+                        } ${column.id !== 'checkbox' ? styles.cell__medium : null}`}
+                      >
+                        {column.id === 'transaction_type_id'
+                          ? column.format(value, order.payment_id)
+                          : column.format && typeof value === 'number'
+                            ? column.format(value)
+                            : value}
+                        {column.id === 'checkbox' &&
+                        isPaymentPending(
+                          transactions[i].payment_id,
+                          transactions[i].transaction_type_id
+                        ) ? (
+                            <Checkbox
+                              size='small'
+                              checked={checkIsSelected(transactions[i].transaction_id)}
+                              onClick={() =>
+                                onSelectOrder(
+                                  transactions[i].transaction_id,
+                                  transactions[i].transaction_amount
+                                )
+                              }
+                            />
+                          ) : null}
+                        {column.id === 'actions' &&
+                        isPaymentPending(
+                          transactions[i].payment_id,
+                          transactions[i].transaction_type_id
+                        ) ? (
+                            <Button className={styles.button__edit}>
+                            Edit Order
+                            </Button>
+                          ) : null}
+                      </TableCell>
+                    );
+                  })}
+                </TableRow>
+              );
+            })}
+            {emptyRows().map((row) => {
+              return (
+                <TableRow key={row} tabIndex={-1}>
+                  {columns.map((column) => {
+                    return <TableCell key={column.id} className={styles.cell} />;
+                  })}
+                </TableRow>
+              );
+            })}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TablePagination
+                className={styles.pagination}
+                count={totalRows}
+                page={currentPage}
+                rowsPerPage={rowsPerPage}
+                labelDisplayedRows={({ page }) => `Page ${page + 1} of ${totalPages}`}
+                rowsPerPageOptions={[rowsPerPage]}
+                onChangePage={(event, page) => onChangePage(page)}
+              />
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </TableContainer>
     </Card>
   );
 };
