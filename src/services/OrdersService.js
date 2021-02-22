7389e52908dd0a8ba6f6c34cba6ca23c4847cd0a
Justin Wong <justinc.s.wong@gmail.com>
2021-02-21 23:26:39 -0800

    SNAK-144-Fix Compile Error

--- src/services/OrdersService.js
+++ src/services/OrdersService.js
@@ -1,18 +1,4 @@
 import httpClient from './axios.config.js';
-import { mockDBCall } from '../mockServer';
-
-const getOrders = (page, rowsPerPage) => {
-  // TODO: Replace with API Call (Ticket: SNAK-144)
-  // try {
-  //   const response = await httpClient.get(`/users/${userId}/transactions`, {
-  //     params: { page: page, size: rowsPerPage }
-  //   });
-  // } catch (err) {
-  //   // TODO: Handle 404
-  //   console.log(err.toString());
-  // }
-  return mockDBCall('transactions', page, rowsPerPage);
-};
 
 const makePayment = async (userId, transactionIds, paymentAmount, processor) => {
   const { data } = await httpClient.post('/payments', {
@@ -36,14 +22,10 @@ const claimPendingOrders = (approvedOrderIds, declinedOrderIds) => {
   throw new Error('Not Implemented!', approvedOrderIds, declinedOrderIds);
 };
 
-<<<<<<< HEAD
-export { getOrders, makePayment, claimPendingOrders };
-=======
 const makeOrder = (userId, transactionTypeId, snackId, transactionAmount, quantity) => {
-  //TODO: API call for add neq transactions 
+  //TODO: API call for add neq transactions
   console.log('Snack purchased!');
   console.log(userId, transactionTypeId, snackId, transactionAmount, quantity);
 };
 
-export { getUserOrders, makePayment, claimPendingOrders, makeOrder};
->>>>>>> dev
+export { makePayment, claimPendingOrders, makeOrder };
