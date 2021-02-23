542b533091c3b06955a331ce77a63e0e0af672d2
Justin Wong <justinc.s.wong@gmail.com>
2021-02-22 23:11:41 -0800

    SNAK-234-Update Mock PendingOrders

--- src/mockPendingOrders.js
+++ src/mockPendingOrders.js
@@ -1,24 +1,17 @@
 const mockPendingOrders = [
   {
     transaction_id: 15,
-    transaction_dtm: '2021-02-21T03:27:51Z',
-    snack_name: 'Banana',
-    quantity: 12,
-    transaction_amount: 300
+    transaction_dtm: '2021-02-25T03:27:51Z',
+    snack_name: 'Coffee Crisp',
+    quantity: 1,
+    transaction_amount: 125
   },
   {
     transaction_id: 12,
-    transaction_dtm: '2021-02-21T03:27:51Z',
-    snack_name: 'Banana',
-    quantity: 12,
-    transaction_amount: 300
-  },
-  {
-    transaction_id: 2,
-    transaction_dtm: '2021-02-21T03:27:51Z',
-    snack_name: 'Banana',
-    quantity: 12,
-    transaction_amount: 300
+    transaction_dtm: '2021-02-25T03:27:51Z',
+    snack_name: 'Tomato',
+    quantity: 2,
+    transaction_amount: 100
   }
 ];
 
