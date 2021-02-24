97966d607335a5e408b168cecc7965a5879e78e0
Justin Wong <justinc.s.wong@gmail.com>
2021-02-23 22:16:37 -0800

    Hotfix: Change mock pending order

--- src/mockPendingOrders.js
+++ src/mockPendingOrders.js
@@ -9,9 +9,9 @@ const mockPendingOrders = [
   {
     transaction_id: 12,
     transaction_dtm: '2021-02-25T03:27:51Z',
-    snack_name: 'Tomato',
-    quantity: 2,
-    transaction_amount: 100
+    snack_name: 'The Marshmallow that Bob Left On The Table Last Week ',
+    quantity: 1,
+    transaction_amount: 2000
   }
 ];
 
