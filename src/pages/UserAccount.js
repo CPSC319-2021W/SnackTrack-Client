77b4f678a5918e0b609847c4e359789036669fea
angelli <angellidimatulac@gmail.com>
2021-03-04 18:04:17 -0800

    Add route paths

--- src/pages/UserAccount.js
+++ src/pages/UserAccount.js
@@ -1,13 +1,16 @@
-import { Container } from '@material-ui/core';
 import React from 'react';
+import { useParams } from 'react-router-dom';
+
+import { Container } from '@material-ui/core';
 import styles from '../styles/Layout.module.css';
 
 const Fallback = () => {
+  const { id } = useParams();
 
   return (
     <div className={styles.base}>
       <Container fixed className={styles.content}>
-        <h4>Account</h4>
+        <h4>Account: { id }</h4>
       </Container>
     </div>
   );
