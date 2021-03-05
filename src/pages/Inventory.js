f8250b000c23582536186a076234576f1a1c54f8
angelli <angellidimatulac@gmail.com>
2021-03-05 12:55:11 -0800

    Removing extra containers

--- src/pages/Inventory.js
+++ src/pages/Inventory.js
@@ -1,14 +1,10 @@
 import React from 'react';
-
-import { Container } from '@material-ui/core';
 import styles from '../styles/Layout.module.css';
 
 const Inventory = () => {
   return (
-    <div className={styles.base}>
-      <Container fixed className={styles.content}>
-        <h4>Inventory</h4>
-      </Container>
+    <div className={styles.content}>
+      <h4>Inventory</h4>
     </div>
   );
 };
--- src/pages/UserAccount.js
+++ src/pages/UserAccount.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
-import { Container } from '@material-ui/core';
 import styles from '../styles/Layout.module.css';
 
 const UserAccount = () => {
   const { id } = useParams();
 
   return (
-    <div className={styles.base}>
-      <Container fixed className={styles.content}>
-        <h4>Account: { id }</h4>
-      </Container>
+    <div className={styles.content}>
+      <h4>Account: { id }</h4>
     </div>
   );
 };
