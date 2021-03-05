06560ba4caf8cbc993997b598067f92bb5c714bc
angelli <angellidimatulac@gmail.com>
2021-03-04 22:20:49 -0800

    Finishing up admin top bar

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -1,6 +1,6 @@
 import { AppBar, Button, Container } from '@material-ui/core';
-import { React, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { Fragment, React, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import NumberFormat from 'react-number-format';
 import { ROUTES } from '../constants';
@@ -13,16 +13,19 @@ import styles from '../styles/HeaderBar.module.css';
 import { useHistory } from 'react-router-dom';
 
 import { ReactComponent as AdminIcon } from '../assets/icons/user_admin.svg';
+import { ReactComponent as DashboardIcon } from '../assets/icons/dashboard.svg';
 import { ReactComponent as EmployeeIcon } from '../assets/icons/user_employee.svg';
 import { ReactComponent as HomeIcon } from '../assets/icons/home.svg';
+import { ReactComponent as InventoryIcon } from '../assets/icons/box.svg';
 import { ReactComponent as LogoutIcon } from '../assets/icons/logout.svg';
 import { ReactComponent as TransactionsIcon } from '../assets/icons/receipt.svg';
+import { ReactComponent as UserGroupIcon } from '../assets/icons/usergroup.svg';
 
 const HeaderBar = (props) => {
   const dispatch = useDispatch();
   const history = useHistory();
   const { isAdminRoute, balance, firstName, handleLogOut, clientid } = props;
-  // const { isAdmin } = useSelector((state) => state.usersReducer.profile);
+  const { isAdmin } = useSelector((state) => state.usersReducer.profile);
   const setProfile = (profile) => dispatch(setUser(profile));
 
   const { pathname } = history.location;
@@ -42,51 +45,109 @@ const HeaderBar = (props) => {
     }
   }, []);
 
+  const renderEmployeeMenu = () => {
+    return (
+      <div className={styles.menu__container__left}>
+        <button
+          className={styles.icon__container}
+          onClick={() => history.push(ROUTES.SNACKS)}
+        >
+          <HomeIcon
+            className={classNames({
+              [styles.unselectable]: true,
+              [styles.icon__base]: true,
+              [styles.icon__active]: pathname === ROUTES.SNACKS
+            })}
+          />
+        </button>
+        <button
+          className={styles.icon__container}
+          onClick={() => history.push(ROUTES.TRANSACTIONS)}
+        >
+          <TransactionsIcon
+            className={classNames({
+              [styles.unselectable]: true,
+              [styles.icon__base]: true,
+              [styles.icon__active]: pathname === ROUTES.TRANSACTIONS
+            })}
+          />
+        </button>
+      </div>
+    );
+  };
+
+  const renderAdminMenu = () => {
+    return (
+      <div className={styles.menu__container__left}>
+        <button
+          className={styles.icon__container}
+          onClick={() => history.push(ROUTES.ADMIN)}
+        >
+          <DashboardIcon
+            className={classNames({
+              [styles.unselectable]: true,
+              [styles.icon__base]: true,
+              [styles.icon__active]: pathname === ROUTES.ADMIN
+            })}
+          />
+        </button>
+        <button
+          className={styles.icon__container}
+          onClick={() => history.push(ROUTES.INVENTORY)}
+        >
+          <InventoryIcon
+            className={classNames({
+              [styles.unselectable]: true,
+              [styles.icon__base]: true,
+              [styles.icon__active]: pathname === ROUTES.INVENTORY
+            })}
+          />
+        </button>
+        <button
+          className={styles.icon__container}
+          onClick={() => history.push(ROUTES.USERS)}
+        >
+          <UserGroupIcon
+            className={classNames({
+              [styles.unselectable]: true,
+              [styles.icon__base]: true,
+              [styles.icon__active]: pathname === ROUTES.USERS
+            })}
+          />
+        </button>
+      </div>
+    );
+  };
+
   return (
     <AppBar className={styles.header}>
       {isAuthenticated() ? (
         <Container className={styles.bar}>
-          <div className={styles.menu__container__left}>
-            <button
-              className={styles.icon__container}
-              onClick={() => history.push(ROUTES.SNACKS)}
-            >
-              <HomeIcon
-                className={classNames({
-                  [styles.unselectable]: true,
-                  [styles.icon__base]: true,
-                  [styles.icon__active]: pathname === ROUTES.SNACKS
-                })}
-              />
-            </button>
-            <button
-              className={styles.icon__container}
-              onClick={() => history.push(ROUTES.TRANSACTIONS)}
-            >
-              <TransactionsIcon
-                className={classNames({
-                  [styles.unselectable]: true,
-                  [styles.icon__base]: true,
-                  [styles.icon__active]: pathname === ROUTES.TRANSACTIONS
-                })}
-              />
-            </button>
-          </div>
-          <div className={styles.label}>
-            <h6 className={styles.label__text}>Total Amount Owing</h6>
-            <h4 className={styles.label__balance}>
-              <NumberFormat
-                value={balance / 100}
-                displayType={'text'}
-                decimalScale={2}
-                fixedDecimalScale={true}
-                prefix={'$'}
-              />
-            </h4>
-          </div>
+          <Fragment>
+            { isAdmin && isAdminRoute
+              ? renderAdminMenu()
+              : (
+                <Fragment>
+                  { renderEmployeeMenu() }
+                  <div className={styles.label}>
+                    <h6 className={styles.label__text}>Total Amount Owing</h6>
+                    <h4 className={styles.label__balance}>
+                      <NumberFormat
+                        value={balance / 100}
+                        displayType={'text'}
+                        decimalScale={2}
+                        fixedDecimalScale={true}
+                        prefix={'$'}
+                      />
+                    </h4>
+                  </div>
+                </Fragment>
+              )
+            }
+          </Fragment>
           <div className={styles.menu__container__right}>
             {
-              true ? (
+              isAdmin ? (
                 isAdminRoute ? (
                   <button
                     className={styles.icon__container}
