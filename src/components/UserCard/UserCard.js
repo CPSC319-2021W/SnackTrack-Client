54a2ffb0f75c6f1abd979d1b82eefb214a80e2aa
Justin Wong <justinc.s.wong@gmail.com>
2021-03-10 00:02:58 -0800

    SNAK-276-Style Card

--- src/components/UserCard/UserCard.js
+++ src/components/UserCard/UserCard.js
@@ -1,20 +1,22 @@
-import defaultAvatar from '../../images/illustrations/defaultAvatar.svg';
-
-import styles from '../../styles/UserCard.module.css';
-
 import { Card, CardActionArea, CardMedia } from '@material-ui/core';
 
+import { ROUTES } from '../../constants';
 import React from 'react';
+import classNames from 'classnames';
+import defaultAvatar from '../../images/illustrations/defaultAvatar.svg';
+import { simpleLogin } from '../../redux/features/users/usersSlice';
+import styles from '../../styles/UserCard.module.css';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-
-import { ROUTES } from '../../constants';
-import { simpleLogin } from '../../redux/features/users/usersSlice';
 
 const UserCard = (props) => {
   const dispatch = useDispatch();
   const history = useHistory();
   const { user } = props;
+  const { pathname } = history.location;
+  const routeToMatch = ROUTES.USERS.split('/').join('\\/');
+  const regex = new RegExp(`(${routeToMatch}/[0-9]+){1,1}`);
+  const noHover = Boolean(pathname.match(regex));
 
   const login = () => {
     dispatch(simpleLogin(user));
@@ -24,8 +26,17 @@ const UserCard = (props) => {
   let img = typeof user.image_uri === 'undefined' ? defaultAvatar : user.image_uri;
 
   return (
-    <Card variant={'outlined'} className={styles.base}>
-      <CardActionArea className={styles.action_area} onClick={login}>
+    <Card
+      variant={'outlined'}
+      className={classNames({ [styles.base]: !noHover, [styles.base__noHover]: noHover })}
+    >
+      <CardActionArea
+        className={classNames({
+          [styles.action_area]: true,
+          [styles.action_area__unclickable]: noHover
+        })}
+        onClick={!noHover ? login : null}
+      >
         <CardMedia
           className={styles.image}
           title={user.first_name}
