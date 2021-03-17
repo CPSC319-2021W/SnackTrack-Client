76a2607b46e4fbf39a1fe9f9aba6518a68f7abc9
Justin Wong <justinc.s.wong@gmail.com>
2021-03-17 00:02:45 -0700

    SNAK-347-Add loading state to upload image button

--- src/components/ImageUploader.js
+++ src/components/ImageUploader.js
@@ -1,4 +1,4 @@
-import { React, useRef } from 'react';
+import { React, useRef, useState } from 'react';
 
 import AppButton from '../components/AppButton';
 import Jimp from 'jimp/es';
@@ -8,6 +8,7 @@ import { useDispatch } from 'react-redux';
 
 const ImageUploader = () => {
   const dispatch = useDispatch();
+  const [isUploading, setIsUploading] = useState(false);
   const uploadedImage = useRef(null);
   const imageUploader = useRef(null);
 
@@ -16,6 +17,7 @@ const ImageUploader = () => {
   };
 
   const handleImageUpload = async (e) => {
+    setIsUploading(true);
     const [file] = e.target.files;
     if (file) {
       const reader = new FileReader();
@@ -23,25 +25,24 @@ const ImageUploader = () => {
       current.file = file;
       reader.onload = async () => {
         try {
-          Jimp.read(reader.result)
-            .then(async (image) => {
-              const img = await image
-                .cover(250, 250)
-                .quality(100)
-                .getBase64Async(Jimp.MIME_PNG);
+          Jimp.read(reader.result).then(async (image) => {
+            const img = await image
+              .cover(250, 250)
+              .quality(100)
+              .getBase64Async(Jimp.MIME_PNG);
 
-              setImageUploadData(img);
-              current.src = img;
-              return;
-            })
-            .catch((err) => {
-              console.error(err);
-            });
+            setImageUploadData(img);
+            current.src = img;
+          });
         } catch (err) {
           console.log(err);
+        } finally {
+          setIsUploading(false);
         }
       };
       reader.readAsDataURL(file);
+    } else {
+      setIsUploading(false);
     }
   };
 
@@ -56,7 +57,7 @@ const ImageUploader = () => {
         </div>
       ) : (
         <div
-          className={styles.imageUploadBox}
+          className={styles.imageUploadGreyBox}
           onClick={() => imageUploader.current.click()}
         />
       )}
@@ -70,7 +71,11 @@ const ImageUploader = () => {
         onChange={handleImageUpload}
       />
       <div className={styles.buttonContainer}>
-        <AppButton secondary onClick={() => imageUploader.current.click()}>
+        <AppButton
+          secondary
+          loading={isUploading}
+          onClick={() => imageUploader.current.click()}
+        >
           Upload Photo
         </AppButton>
       </div>
