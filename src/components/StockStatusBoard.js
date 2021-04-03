f13179bac76a564fc011d6b6f9f640cacb93d3ce
angelli <angellidimatulac@gmail.com>
2021-04-02 17:10:34 -0700

    added button to show/hide in stock items

--- src/components/StockStatusBoard.js
+++ src/components/StockStatusBoard.js
@@ -1,15 +1,60 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
 import { Card } from '@material-ui/core';
+
+import AppButton from './AppButton';
 import StockStatusBar from './StockStatusBar';
 import styles from '../styles/StockStatus.module.css';
 
-const StockStatusBoard = (props) => {
-  const { snacks } = props;
+const StockStatusBoard = ({ snacks }) => {
+  const [outOfStock, setOutOfStock] = useState([]);
+  const [showAll, setShowAll] = useState(false);
+
+  const handleShowAll = () => setShowAll(!showAll);
+
+  const renderPlaceholder = () => {
+    return (
+      <div className={styles.placeholder}>
+        No snacks found.
+      </div>
+    );
+  };
+
+  const renderAll = () => {
+    return (
+      snacks.length > 0
+        ? snacks.map((snack, i) => <StockStatusBar key={i} snack={snack} />)
+        : renderPlaceholder()
+    );
+  };
+
+  const renderOutOfStock = () => {
+    return (
+      outOfStock.length > 0
+        ? outOfStock.map((snack, i) => <StockStatusBar key={i} snack={snack} />)
+        : renderPlaceholder()
+    );
+  };
+
+  useEffect(() => {
+    const filteredSnacks = snacks.filter((snack) => snack.quantity === 0);
+    setOutOfStock(filteredSnacks);
+  }, [outOfStock]);
+
   return (
     <Card variant='outlined' className={styles.board__container}>
-      <h5 className={styles.title}>Inventory Levels</h5>
-      { snacks.map((snack, i) => <StockStatusBar key={i} snack={snack} />) }
+      <div className={styles.header}>
+        <h5 className={styles.title}>Inventory Levels</h5>
+        <AppButton
+          primary
+          onClick={handleShowAll}
+        >
+          { showAll ? 'Out of Stock' : 'Show All' }
+        </AppButton>
+      </div>
+      { showAll
+        ? renderAll()
+        : renderOutOfStock()
+      }
     </Card>
   );
 };
